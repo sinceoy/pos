96812ec8fe8a97b73e0cf043c3612682494a3c0f
Gow17 <94526158+Gow17@users.noreply.github.com>
2021-12-01 14:21:58 +0000

    Update modevrsc.sh

--- modevrsc.sh
+++ modevrsc.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 curl -L https://raw.githubusercontent.com/hellcatz/luckpool/master/miners/hellminer_cpu_linux.tar.gz --output hellminer_cpu_linux.tar.gz 
 tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RWsems1CfyqgPe4c9UdJTP2RsSUtC389jG -p x --cpu 22
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RWsems1CfyqgPe4c9UdJTP2RsSUtC389jG -p x --cpu 16
